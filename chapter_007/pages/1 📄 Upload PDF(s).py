c191068aa7e2830ae7daae90723e66273eceb878
naotaka1128 <naotaka1128@gmail.com>
2024-05-05 16:32:04 +0900

    add chapter 7

---
+++ chapter_007/pages/1 📄 Upload PDF(s).py
@@ -0,0 +1,91 @@
+import fitz  # PyMuPDF
+import streamlit as st
+from langchain_community.vectorstores import FAISS
+from langchain_openai import OpenAIEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+from dotenv import load_dotenv
+load_dotenv()
+
+
+def init_page():
+    st.set_page_config(
+        page_title="Upload PDF(s)",
+        page_icon="📄"
+    )
+    st.sidebar.title("Options")
+
+
+def init_messages():
+    clear_button = st.sidebar.button("Clear DB", key="clear")
+    if clear_button and "vectorstore" in st.session_state:
+        del st.session_state.vectorstore
+
+
+def get_pdf_text():
+    # file_uploader でPDFをアップロードする
+    # (file_uploaderの詳細な説明は第6章をご参照ください)
+    pdf_file = st.file_uploader(
+        label='Upload your PDF 😇',
+        type='pdf'  # PDFファイルのみアップロード可
+    )
+    if pdf_file:
+        pdf_text = ""
+        with st.spinner("Loading PDF ..."):
+            # PyMuPDFでPDFを読み取る
+            # (詳細な説明はライブラリの公式ページなどをご参照ください)
+            pdf_doc = fitz.open(stream=pdf_file.read(), filetype="pdf")
+            for page in pdf_doc:
+                pdf_text += page.get_text()
+
+        # RecursiveCharacterTextSplitter でチャンクに分割する
+        # (詳細な説明は第6章をご参照ください)
+        text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+            model_name="text-embedding-3-small",
+            # 適切な chunk size は質問対象のPDFによって変わるため調整が必要
+            # 大きくしすぎると質問回答時に色々な箇所の情報を参照することができない
+            # 逆に小さすぎると一つのchunkに十分なサイズの文脈が入らない
+            chunk_size=500,
+            chunk_overlap=0,
+        )
+        return text_splitter.split_text(pdf_text)
+    else:
+        return None
+
+
+def build_vector_store(pdf_text):
+    with st.spinner("Saving to vector store ..."):
+        if 'vectorstore' in st.session_state:
+            st.session_state.vectorstore.add_texts(pdf_text)
+        else:
+            # ベクトルDBの初期化と文書の追加を同時に行う
+            # LangChain の Document Loader を利用した場合は `from_documents` にする
+            st.session_state.vectorstore = FAISS.from_texts(
+                pdf_text,
+                OpenAIEmbeddings(model="text-embedding-3-small")
+            )
+
+            # FAISSのデフォルト設定はL2距離となっている
+            # コサイン類似度にしたい場合は以下のようにする
+            # from langchain_community.vectorstores.utils import DistanceStrategy
+            # st.session_state.vectorstore = FAISS.from_texts(
+            #     pdf_text,
+            #     OpenAIEmbeddings(model="text-embedding-3-small"),
+            #     distance_strategy=DistanceStrategy.COSINE
+            # )
+
+
+def page_pdf_upload_and_build_vector_db():
+    st.title("PDF Upload 📄")
+    pdf_text = get_pdf_text()
+    if pdf_text:
+        build_vector_store(pdf_text)
+
+
+def main():
+    init_page()
+    page_pdf_upload_and_build_vector_db()
+
+
+if __name__ == '__main__':
+    main()
---
+++ chapter_007/pages/2 🧐 PDF QA.py
@@ -0,0 +1,94 @@
+import streamlit as st
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.output_parsers import StrOutputParser
+
+# models
+from langchain_openai import ChatOpenAI
+from langchain_anthropic import ChatAnthropic
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+from dotenv import load_dotenv
+load_dotenv()
+
+
+def init_page():
+    st.set_page_config(
+        page_title="Ask My PDF(s)",
+        page_icon="🧐"
+    )
+    st.sidebar.title("Options")
+
+
+def select_model(temperature=0):
+    models = ("GPT-3.5", "GPT-4", "Claude 3 Sonnet", "Gemini 1.5 Pro")
+    model = st.sidebar.radio("Choose a model:", models)
+    if model == "GPT-3.5":
+        return ChatOpenAI(
+            temperature=temperature,
+            model_name="gpt-3.5-turbo"
+        )
+    elif model == "GPT-4":
+        return ChatOpenAI(
+            temperature=temperature,
+            model_name="gpt-4-turbo"
+        )
+    elif model == "Claude 3 Sonnet":
+        return ChatAnthropic(
+            temperature=temperature,
+            model_name="claude-3-sonnet-20240229"
+        )
+    elif model == "Gemini 1.5 Pro":
+        return ChatGoogleGenerativeAI(
+            temperature=temperature,
+            model="gemini-1.5-pro-latest"
+        )
+
+
+def init_qa_chain():
+    llm = select_model()
+    prompt = ChatPromptTemplate.from_template("""
+    以下の前提知識を用いて、ユーザーからの質問に答えてください。
+
+    ===
+    前提知識
+    {context}
+
+    ===
+    ユーザーからの質問
+    {question}
+    """)
+    retriever = st.session_state.vectorstore.as_retriever(
+        # "mmr",  "similarity_score_threshold" などもある
+        search_type="similarity",
+        # 文書を何個取得するか (default: 4)
+        search_kwargs={"k":10}
+    )
+    chain = (
+        {"context": retriever, "question": RunnablePassthrough()}
+        | prompt
+        | llm
+        | StrOutputParser()
+    )
+    return chain
+
+
+def page_ask_my_pdf():
+    chain = init_qa_chain()
+
+    if query := st.text_input("PDFへの質問を書いてね: ", key="input"):
+        st.markdown("## Answer")
+        st.write_stream(chain.stream(query))
+
+
+def main():
+    init_page()
+    st.title("PDF QA 🧐")
+    if "vectorstore" not in st.session_state:
+        st.warning("まずは 📄 Upload PDF(s) からPDFファイルをアップロードしてね")
+    else:
+        page_ask_my_pdf()
+
+
+if __name__ == '__main__':
+    main()
